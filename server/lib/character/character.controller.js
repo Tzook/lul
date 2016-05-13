feaeee81b2a1b3f298652c5cdca2e7459932ea63
tzook.shaked <tzook.shaked@myheritage.com>
2016-05-13 16:55:07 +0300

    Send characters instead of user after adding a character

--- server/lib/character/character.controller.js
+++ server/lib/character/character.controller.js
@@ -12,7 +12,7 @@ class CharacterController extends MasterController {
     handleNewCharacter(req, res, next) {
         return this.services.saveNewCharacter(req.user, req.body)
         .then(d => {
-            this.sendData(res, this.LOGS.CHARACTER_CREATED_SUCCESSFULLY, req.user);
+            this.sendData(res, this.LOGS.CHARACTER_CREATED_SUCCESSFULLY, req.user.characters);
         })
         .catch(e => {
             this.sendError(res, this.LOGS.MASTER_INTERNAL_ERROR, {e, fn: "handleNewCharacter", file: "character.controller.js"});
