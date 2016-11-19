becdf3aa7201afd9c687a74fef2130329d2eb642
tzook.shaked <tzook.shaked@myheritage.com>
2016-11-19 21:58:27 +0200

    Add whisper/global/room chats

--- server/lib/chat/chat.router.ts
+++ server/lib/chat/chat.router.ts
@@ -4,10 +4,26 @@ let SERVER_GETS		   = require('../../../server/lib/chat/chat.config.json').SERVE
 
 export default class ChatRouter extends SocketioRouterBase {
 	[SERVER_GETS.MESSAGE](data, socket) {
+		let emit = to => {
+			to.emit(this.CLIENT_GETS.MESSAGE, {
+				id: socket.character._id,
+				message: data.message,
+				type: data.type
+			});
+		}
+
 		console.log("Got message", data);
-		socket.broadcast.to(socket.character.room).emit(this.CLIENT_GETS.MESSAGE, {
-			id: socket.character._id,
-			message: data.message
-		});
+		if (data.type === "whisper" && data.target_id) {
+			let targetSocket = socket.map.get(data.target_id);
+			if (!targetSocket) {
+				console.error("Failed to find socket!", data);
+			} else {
+				emit(targetSocket);
+			}
+		} else if (data.type === "global") {
+			emit(socket.broadcast);
+		} else { // scene
+			emit(socket.broadcast.to(socket.character.room));
+		}
 	}
 };
