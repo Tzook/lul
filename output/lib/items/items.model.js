7f1c86dc90d5043fef56fe85dfd77b3c6f24817f
tzook.shaked <tzook.shaked@myheritage.com>
2016-12-03 16:25:15 +0200

    Create items model

--- output/lib/items/items.model.js
+++ output/lib/items/items.model.js
@@ -1,8 +1,22 @@
 'use strict';
 const master_model_1 = require('../master/master.model');
 class ItemsModel extends master_model_1.default {
+    init(files, app) {
+        this.schema = {
+            name: String,
+            icon: String,
+        };
+    }
+    get priority() {
+        return 25;
+    }
+    createModel() {
+        this.setModel('Items');
+        this.addToSchema('Character', { items: [this.getModel().schema] });
+        return Promise.resolve();
+    }
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ItemsModel;
 ;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaXRlbXMubW9kZWwuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zZXJ2ZXIvbGliL2l0ZW1zL2l0ZW1zLm1vZGVsLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBLFlBQVksQ0FBQztBQUNiLCtCQUF3Qix3QkFBd0IsQ0FBQyxDQUFBO0FBRWpELHlCQUF3QyxzQkFBVztBQUVuRCxDQUFDO0FBRkQ7NEJBRUMsQ0FBQTtBQUFBLENBQUMifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaXRlbXMubW9kZWwuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zZXJ2ZXIvbGliL2l0ZW1zL2l0ZW1zLm1vZGVsLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBLFlBQVksQ0FBQztBQUNiLCtCQUF3Qix3QkFBd0IsQ0FBQyxDQUFBO0FBRWpELHlCQUF3QyxzQkFBVztJQUMvQyxJQUFJLENBQUMsS0FBSyxFQUFFLEdBQUc7UUFDWCxJQUFJLENBQUMsTUFBTSxHQUFHO1lBQ1YsSUFBSSxFQUFFLE1BQU07WUFDWixJQUFJLEVBQUUsTUFBTTtTQUNmLENBQUM7SUFDTixDQUFDO0lBRUQsSUFBSSxRQUFRO1FBQ1IsTUFBTSxDQUFDLEVBQUUsQ0FBQztJQUNkLENBQUM7SUFFRCxXQUFXO1FBQ1AsSUFBSSxDQUFDLFFBQVEsQ0FBQyxPQUFPLENBQUMsQ0FBQztRQUN2QixJQUFJLENBQUMsV0FBVyxDQUFDLFdBQVcsRUFBRSxFQUFDLEtBQUssRUFBRSxDQUFDLElBQUksQ0FBQyxRQUFRLEVBQUUsQ0FBQyxNQUFNLENBQUMsRUFBQyxDQUFDLENBQUM7UUFDakUsTUFBTSxDQUFDLE9BQU8sQ0FBQyxPQUFPLEVBQUUsQ0FBQztJQUM3QixDQUFDO0FBQ0wsQ0FBQztBQWpCRDs0QkFpQkMsQ0FBQTtBQUFBLENBQUMifQ==
--- server/lib/items/items.model.ts
+++ server/lib/items/items.model.ts
@@ -2,5 +2,20 @@
 import MasterModel from '../master/master.model';
 
 export default class ItemsModel extends MasterModel {
+    init(files, app) {
+        this.schema = {
+            name: String,
+            icon: String,
+        };
+    }
 
+    get priority() {
+        return 25;
+    }
+
+    createModel() {
+        this.setModel('Items');
+        this.addToSchema('Character', {items: [this.getModel().schema]});
+        return Promise.resolve();
+    }
 };
