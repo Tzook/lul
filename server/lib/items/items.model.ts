143c601f2cf9755737d30fd751d90360259f12b6
tzook.shaked <tzook.shaked@myheritage.com>
2016-12-24 12:10:33 +0200

    Initiallize characters with 20 empty items

--- server/lib/items/items.model.ts
+++ server/lib/items/items.model.ts
@@ -1,5 +1,6 @@
 "use strict";
 import MasterModel from "../master/master.model";
+let config = require('../../../server/lib/items/items.config.json');
 
 export default class ItemsModel extends MasterModel {
     init(files, app) {
@@ -18,12 +19,16 @@ export default class ItemsModel extends MasterModel {
     createModel() {
         this.setModel("Item");
         this.addToSchema("Character", {items: [this.getModel().schema]});
-        this.listenForFieldAddition("Character", "items", [
+        let items = [
 			new this.model({
 				name: "Sword of Elad",
 				icon: "sword_of_elad"
 			})
-		]);
+		];
+        for (var i = 1; i < config.MAX_ITEMS; i++) {
+            items[i] = "";
+        }
+        this.listenForFieldAddition("Character", "items", items);
         return Promise.resolve();
     }
 };
