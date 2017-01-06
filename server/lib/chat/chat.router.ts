923e82645a64d9ab690c6fd7539651cc8ddb7963
tzook.shaked <tzook.shaked@myheritage.com>
2017-01-06 13:44:39 +0200

    Update chat events

--- server/lib/chat/chat.router.ts
+++ server/lib/chat/chat.router.ts
@@ -3,27 +3,36 @@ import SocketioRouterBase from '../socketio/socketio.router.base';
 let SERVER_GETS		   = require('../../../server/lib/chat/chat.config.json').SERVER_GETS;
 
 export default class ChatRouter extends SocketioRouterBase {
-	[SERVER_GETS.MESSAGE](data, socket) {
-		let emit = to => {
-			to.emit(this.CLIENT_GETS.MESSAGE, {
-				id: socket.character._id,
-				message: data.message,
-				type: data.type
-			});
-		}
+	[SERVER_GETS.SHOUT](data, socket) {
+		console.log("Emitting shout", data);
+		socket.broadcast.emit(this.CLIENT_GETS.SHOUT, {
+			id: socket.character._id,
+			msg: data.msg,
+		});
+	}
 
-		console.log("Got message", data);
-		if (data.type === "whisper" && data.target_id) {
+	[SERVER_GETS.CHAT](data, socket) {
+		console.log("Emitting chat", data);
+		socket.broadcast.to(socket.character.room).emit(this.CLIENT_GETS.CHAT, {
+			id: socket.character._id,
+			msg: data.msg,
+		});
+	}
+
+	[SERVER_GETS.WHISPER](data, socket) {
+		if (typeof data.to_id == 'string') {
 			let targetSocket = socket.map.get(data.target_id);
 			if (!targetSocket) {
-				console.error("Failed to find socket!", data);
+				console.error("Failed to find socket for whisper!", data);
 			} else {
-				emit(targetSocket);
+				console.log("Emitting whisper", data);
+				targetSocket.emit(this.CLIENT_GETS.WHISPER, {
+					id: socket.character._id,
+					msg: data.msg,
+				});
 			}
-		} else if (data.type === "global") {
-			emit(socket.broadcast);
-		} else { // scene
-			emit(socket.broadcast.to(socket.character.room));
+		} else {
+			console.log("did not provide to_id in data for whisper!", data);
 		}
 	}
 };
