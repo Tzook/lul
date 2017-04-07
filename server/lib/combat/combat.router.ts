713a722da327bde91e658ba3b786580641aa35ba
Tzook <tzook10@gmail.com>
2017-04-07 19:39:47 +0300

    move taking dmg from stats to combat files

--- server/lib/combat/combat.router.ts
+++ server/lib/combat/combat.router.ts
@@ -1,12 +1,13 @@
 'use strict';
 import SocketioRouterBase from '../socketio/socketio.router.base';
 import CombatMiddleware from './combat.middleware';
-let SERVER_GETS		   = require('../../../server/lib/combat/combat.config.json').SERVER_GETS;
+import * as _ from 'underscore';
+let config = require('../../../server/lib/combat/combat.config.json');
 
 export default class CombatRouter extends SocketioRouterBase {
 	protected middleware: CombatMiddleware;
 
-	[SERVER_GETS.LOAD_ATTACK](data, socket: GameSocket) {
+	[config.SERVER_GETS.LOAD_ATTACK](data, socket: GameSocket) {
 		console.log("loading attack", socket.character.name);
 		socket.broadcast.to(socket.character.room).emit(this.CLIENT_GETS.LOAD_ATTACK, {
 			id: socket.character._id,
@@ -14,7 +15,7 @@ export default class CombatRouter extends SocketioRouterBase {
 		});
 	}
 
-	[SERVER_GETS.PERFORM_ATTACK](data, socket: GameSocket) {
+	[config.SERVER_GETS.PERFORM_ATTACK](data, socket: GameSocket) {
 		let load = this.middleware.getValidLoad(data.load);
 		console.log("performing attack", socket.character.name, data, load);
 		socket.broadcast.to(socket.character.room).emit(this.CLIENT_GETS.PERFORM_ATTACK, {
@@ -22,5 +23,24 @@ export default class CombatRouter extends SocketioRouterBase {
 			ability: socket.character.stats.primaryAbility,
 			load
 		});
+	}
+
+	[config.SERVER_GETS.TAKE_DMG](data, socket: GameSocket) {
+		let absoluteDmg: number = config.DMG[data.from];
+        if (absoluteDmg) {
+			// randomize the dmg
+            let dmg = _.random(absoluteDmg - config.DMG_RANDOM_RANGE, absoluteDmg + config.DMG_RANDOM_RANGE);
+			// make sure that after randomizing, dmg doesn't get negative
+			dmg = Math.max(config.MIN_DMG_TAKEN, dmg); 
+            socket.character.stats.hp.now = Math.max(0, socket.character.stats.hp.now - dmg);
+            this.io.to(socket.character.room).emit(this.CLIENT_GETS.TAKE_DMG, {
+                id: socket.character._id,
+                dmg,
+                hp: socket.character.stats.hp.now
+            });
+            console.log("Taking damage", socket.character.name, dmg, socket.character.stats.hp.now);
+        } else {
+            console.log("Tried to take dmg but invalid params", data.from);
+        }
 	}
 };
