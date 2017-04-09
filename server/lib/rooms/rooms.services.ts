4c63da52ec039fd5d4ce34da56949ccae25f6f83
Tzook <tzook10@gmail.com>
2017-04-09 08:52:13 +0300

    Add ability to create new rooms

--- server/lib/rooms/rooms.services.ts
+++ server/lib/rooms/rooms.services.ts
@@ -2,23 +2,27 @@
 import MasterServices from '../master/master.services';
 
 export default class RoomsServices extends MasterServices {
-	findRoom (roomName) {
-
-	}
-
-	createRoom (roomName) {
-
-	}
-
-	removeRoom (roomName) {
-
-	}
-
-	removeUserFromRoom(userId, roomName) {
-
-	}
-
-	addUserToRoom(userId, roomName) {
-
+	public generateRoom (scene): Promise<any> {
+		console.log("Generating room from scene:", scene);
+		let name = scene.name;
+		let portals = (scene.Portals || []).map(portal => ({
+			target: portal.TargetLevel,
+			x: portal.PositionX,
+			y: portal.PositionY
+		}));
+		let spawns = (scene.Spawners || []).map(spawner => ({
+			mob: spawner.MonsterKey,
+			cap: spawner.SpawnCap,
+			interval: spawner.RespawnTime,
+			x: spawner.PositionX,
+			y: spawner.PositionY
+		}));
+		let room = new this.Model({
+			name,
+			portals,
+			spawns,
+		});
+		
+		return this.Model.findOneAndUpdate({name}, room, {new: true, upsert: true});
 	}
 };
