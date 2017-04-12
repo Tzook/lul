b270ff19af8dcf8475fd83108a02d3bd33035624
Tzook <tzook10@gmail.com>
2017-04-12 08:18:11 +0300

    Add api to delete a user

--- server/lib/user/user.controller.ts
+++ server/lib/user/user.controller.ts
@@ -1,11 +1,21 @@
 'use strict';
 import MasterController from '../master/master.controller';
+import UserServices from "./user.services";
 
 export default class UserController extends MasterController {
+    protected services: UserServices;
 
     sendUser(req, res) {
         req.user.password = undefined; // remove the password
         this.sendData(res, req.LOG || this.LOGS.USER_SESSION_OK, req.user);
+    }
+
+    sendLogout(req, res, next) {
+        this.sendData(res, this.LOGS.USER_LOGGED_OUT_OK);
+    }
+
+    sendDeleted(req, res, next) {
+        this.sendData(res, this.LOGS.USER_DELETED);
     }
 
     performLogin(req, res, next) {
@@ -20,28 +30,35 @@ export default class UserController extends MasterController {
         });
     }
 
-    performLogout(req, res) {
+    performLogout(req, res, next) {
         req.session.destroy(e => {
-            if (!e) {
-                this.sendData(res, this.LOGS.USER_LOGGED_OUT_OK);
-            } else {
+            if (e) {
                 this.sendError(res, this.LOGS.MASTER_INTERNAL_ERROR, {e, fn: "performLogout", file: "user.controller.js"});
+            } else {
+                next();
             }
         });
     }
 
     handleNewUser(req, res, next) {
         return this.services.saveNewUser(req.body)
-        .then(d => {
-            req.body.user = d[0];
-            req.LOG = this.LOGS.USER_REGISTERED_OK;
-            next();
-        })
-        .catch(e => {
-            this.sendError(res, this.LOGS.MASTER_INTERNAL_ERROR, {e, fn: "handleNewUser", file: "user.controller.js"});
-        });
+            .then(d => {
+                req.body.user = d[0];
+                req.LOG = this.LOGS.USER_REGISTERED_OK;
+                next();
+            })
+            .catch(e => {
+                this.sendError(res, this.LOGS.MASTER_INTERNAL_ERROR, {e, fn: "handleNewUser", file: "user.controller.js"});
+            });
     }
 
+    deleteUser(req, res, next) {
+        return this.services.deleteUser(req.user._id)
+            .then(d => next())
+            .catch(e => {
+                this.sendError(res, this.LOGS.MASTER_INTERNAL_ERROR, {e, fn: "deleteUser", file: "user.controller.js"});
+            });
+    }
 
     // PASSPORT FUNCTIONS //
     // ================== //
