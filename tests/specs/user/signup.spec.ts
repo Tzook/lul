8ecb1cbf1a66d227bd6d650477dca71e6b951bc8
Tzook <tzook10@gmail.com>
2017-04-12 08:41:38 +0300

    Automations - fully cover signup

--- tests/specs/user/signup.spec.ts
+++ tests/specs/user/signup.spec.ts
@@ -1,10 +1,10 @@
 import { browser } from "protractor/built";
-import { raiseBrowser, expectText } from "./common";
+import { raiseBrowser, expectText, login, logout, deleteUser } from "../common";
 
 describe('signup', () => {
     raiseBrowser();
 
-    describe('failur', () => {
+    describe('failure', () => {
         it('should not allow registering without a username', () => {
             browser.executeScript(`sendPost('/user/register', {});`);
             expectText("Parameter 'username' is required.");
@@ -15,26 +15,31 @@ describe('signup', () => {
             expectText("Parameter 'username' is required.");
         });
 
+        it('should not allow registering with a username that is too long', () => {
+            browser.executeScript(`sendPost('/user/register', {username: 'averyverylongusernamethatisjusttoomuch'});`);
+            expectText("The parameter 'username' is out of range.");
+        });
+
         it('should not allow registering without a password', () => {
-            browser.executeScript(`sendPost('/user/register', {username: 'testName'});`);
+            browser.executeScript(`sendPost('/user/register', {username: 'uncaughtTestName'});`);
             expectText("Parameter 'password' is required.");
         });
 
         it('should not allow registering with a password that is not a string', () => {
-            browser.executeScript(`sendPost('/user/register', {username: 'testName', password: {}});`);
+            browser.executeScript(`sendPost('/user/register', {username: 'uncaughtTestName', password: {}});`);
             expectText("Parameter 'password' is required.");
         });
 
         it('should not allow registering with a password that is not 32 characters long', () => {
-            browser.executeScript(`sendPost('/user/register', {username: 'testName', password: 'tooshort'});`);
+            browser.executeScript(`sendPost('/user/register', {username: 'uncaughtTestName', password: 'tooshort'});`);
             expectText("parameter 'password' is out of range.");
         });
 
         it('should not allow registering with a user already logged in', () => {
-            browser.executeScript(`sendPost('/user/login', {username: 'test', password: '12345678123456781234567812345678'});`);
-            expectText("Logged in successfully.");
-            browser.executeScript(`sendPost('/user/register', {username: 'testingUser', password: '12345678123456781234567812345678'});`);
+            login();
+            browser.executeScript(`sendPost('/user/register', {username: 'uncaughtTestName', password: '12345678123456781234567812345678'});`);
             expectText("Cannot do this request with a user already logged in.");
+            logout();
         });
 
         it('should not allow registering a username that is already taken', () => {
@@ -43,7 +48,11 @@ describe('signup', () => {
         });
     });
 
-    // describe('success', () => {
-        // TODO add delete API so this can be tested..
-    // })
+    fdescribe('success', () => {
+        it('should create a new user when logged out and has a valid username and password, and log it in automatically', () => {
+            browser.executeScript(`sendPost('/user/register', {username: 'uncaughtTestName', password: '12345678123456781234567812345678'});`);
+            expectText("Registered and then logged in successfully.");
+            deleteUser();
+        });
+    });
 });
