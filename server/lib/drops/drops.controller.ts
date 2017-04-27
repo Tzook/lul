5b57c4daeda005ef0d636c83499f58ecfa3b263f
Tzook <tzook10@gmail.com>
2017-04-27 19:01:32 +0300

    Update mongoose

--- server/lib/drops/drops.controller.ts
+++ server/lib/drops/drops.controller.ts
@@ -12,7 +12,6 @@ export default class DropsController extends MasterController {
     public isDropped(chance: number): boolean {
         let rand = _.random(DROP_PRECISE);
         let rolled = rand * 100 / DROP_PRECISE;
-        console.log(rolled, chance);
         return chance >= rolled;
     }
 };
