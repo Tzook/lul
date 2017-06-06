7e36dc61de9cb74a1cf02bcfba6e8226d6f39b32
Tzook <tzook10@gmail.com>
2017-06-06 07:18:26 +0300

    Add change-ability event. Closes #45

--- server/lib/combat/combat.middleware.ts
+++ server/lib/combat/combat.middleware.ts
@@ -2,7 +2,11 @@
 import MasterMiddleware from '../master/master.middleware';
 
 export default class CombatMiddleware extends MasterMiddleware {
-    getValidLoad(load): number {
+    public getValidLoad(load): number {
         return load >= 1 && load <= 100 ? (load | 0) : 0;
+    }
+
+    public canChangeAbility(socket: GameSocket, ability: string): boolean {
+        return (<any>socket.character.stats.abilities).includes(ability);
     }
 };
--- server/lib/combat/combat.router.ts
+++ server/lib/combat/combat.router.ts
@@ -24,4 +24,20 @@ export default class CombatRouter extends SocketioRouterBase {
 			load
 		});
 	}
+
+	[config.SERVER_GETS.CHANGE_ABILITY.name](data, socket: GameSocket) {
+		let {ability} = data;
+		if (!ability) {
+			this.sendError(data, socket, "Must send what ability to use");
+		} else if (!this.middleware.canChangeAbility(socket, ability)) {
+			this.sendError(data, socket, "Character cannot change to this ability");
+		} else {
+			console.log("changing ability", socket.character.name, ability);
+			socket.character.stats.primaryAbility = ability;
+			socket.broadcast.to(socket.character.room).emit(this.CLIENT_GETS.CHANGE_ABILITY.name, {
+				id: socket.character._id,
+				ability: socket.character.stats.primaryAbility,
+			});
+		}
+	}
 };
