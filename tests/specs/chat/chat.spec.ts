21ae5e92faab4f6db864ee41dcbc99c867e4158b
Noam <noam@mail.com>
2017-06-24 13:14:26 +0300

    adding party chat tests. Closes #52.

--- tests/specs/chat/chat.spec.ts
+++ tests/specs/chat/chat.spec.ts
@@ -1,6 +1,7 @@
 import { expectText, newBrowser } from '../common';
 import { browser } from 'protractor/built';
 import { TEST_CHAR_NAME2, TEST_CHAR_NAME } from '../character/character.common';
+import { createParty, joinParty, leaveParty } from '../party/party.common';
 
 describe('chatting', () => {
     describe('shout', () => {
@@ -21,18 +22,39 @@ describe('chatting', () => {
 
     describe('whisper', () => {
         it('should send an error when other character does not exist', () => {
-            browser.executeScript(`socket.emit("whispered", {msg: "hi", to: "123"});`)
+            browser.executeScript(`socket.emit("whispered", {msg: "hi", to: "123"});`);
             expectText(`"Failed to find socket for whisper"`);
         });
 
         it('should allow to whisper others by name', () => {
-            browser.executeScript(`socket.emit("whispered", {msg: "hi", to: "${TEST_CHAR_NAME2}"});`)
+            browser.executeScript(`socket.emit("whispered", {msg: "hi", to: "${TEST_CHAR_NAME2}"});`);
             expectText(`"whisper"`, newBrowser.instance);
         });
 
         it('should allow to whisper oneself', () => {
-            browser.executeScript(`socket.emit("whispered", {msg: "hi", to: "${TEST_CHAR_NAME}"});`)
+            browser.executeScript(`socket.emit("whispered", {msg: "hi", to: "${TEST_CHAR_NAME}"});`);
             expectText(`"whisper"`);
+        });
+    });
+
+    describe('party_chat' ,() => {
+        beforeAll(() => {
+            createParty(browser);
+            joinParty(browser, newBrowser.instance, TEST_CHAR_NAME, TEST_CHAR_NAME2);
+        });
+        it('should communicate through party chat from leader', () => {
+            browser.executeScript(`socket.emit("party_chatted", {msg: "hi party"});`);
+            expectText(`"hi party"`, newBrowser.instance);
+        });
+        it('should communicate through party chat from member', () => {
+            newBrowser.instance.executeScript(`socket.emit("party_chatted", {msg: "hi party2"});`);
+            expectText(`"hi party2"`, browser);
+        });
+        it('should throw error if char have no party', () => {
+            leaveParty(browser);
+            leaveParty(newBrowser.instance);
+            browser.executeScript(`socket.emit("party_chatted", {msg: "hi party"});`);
+            expectText(`"Failed to find party for engaging in party chat"`);
         });
     });
 });
