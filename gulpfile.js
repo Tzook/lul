16a48e5fcbc5d23b9740851aae52b78ae6ebe8a7
Tzook <tzook10@gmail.com>
2017-06-24 16:04:30 +0300

    Upgrade app to use node 8, while keeping the machine's node version the same

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,34 @@
-'use strict';
-(function() {
-	var gulp 		= require("gulp"),
-	    spawn       = require('child_process').spawn,
-		server 		= require("gulp-develop-server");
+const gulp = require("gulp");
+const spawn = require("child_process").spawn;
+const server = require("gulp-develop-server");
+const ts = require("gulp-typescript");
+const sourcemaps = require('gulp-sourcemaps');
+const tsProject = ts.createProject("tsconfig.json");
 
-	gulp.task("server:start", () => {
-		let ts = spawn('npm', ['start']); // set the typescript compilation process going
-		ts.stdout.on("data", data => {
-			data = data.toString();
-			console.log(data);
-			if (data.indexOf("Compilation complete") !== -1) {
-				startServerWhenReady(data);
-			}
-		});
-	});
+gulp.task("restart", () => {
+    tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .js
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(tsProject.options.outDir))
+        .pipe(server({path: "output/main.js"}));
+});
 
-	let startServerWhenReady = data => {
-		server.listen({path: './output/main.js'});
-		startServerWhenReady = () => {
-			server.restart();
-		};
-	};
+gulp.task("watch", ["restart"], () => {
+    gulp.watch(["server/**/*"], ["restart"]);
+});
 
-	// Server kill
-	gulp.task("server:restart", function() {
-		server.restart();
-	});
+gulp.task("default", () => {
+    let app = spawn("npm", ["start", "--color"], {detached: true});
+    app.stdout.pipe(process.stdout);
+    app.stderr.pipe(process.stderr);
 
-	// WATCH
-	// =====
-	// Run the server unit tests just once, then run the server itself
-	gulp.task("default", ["server:start"], function() {
-		gulp.watch(["server/**/*.json"], ["server:restart"]);
-	});
-})();
+    process.on("SIGINT", terminate);
+    process.on("SIGTERM", terminate);
+
+    function terminate() {
+        // kills the entire processes group
+        process.kill(-app.pid);
+    }
+});
