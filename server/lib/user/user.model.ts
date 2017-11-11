bf8bbff23eb1cb6b68fd28efe729f236f9c79fd7
Tzook <tzook10@gmail.com>
2017-11-11 22:58:45 +0200

    Add explanation comment for schema changes

--- server/lib/user/user.model.ts
+++ server/lib/user/user.model.ts
@@ -17,6 +17,19 @@ export default class UserModel extends MasterModel {
 
     createModel() {
         this.setModel('User');
+        
+        // uncomment this IN LOCAL ONLY to run a modification on all characters, like schema changes
+        // this.getModel().find({})
+        //     .then(users => {
+        //         for (let i = 0; i < users.length; i++) {
+        //             let user = users[i];
+        //             for (let j = 0; j < user.characters.length; j++) {
+        //                 let char = user.characters[j];
+        //             }
+        //             // user.save();
+        //         }
+        //     });
+
         this.removeListen('User');
         return Promise.resolve();
     }
