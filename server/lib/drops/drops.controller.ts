e11d410b6abb81c15e739008a7808182d7782248
Tzook <tzook10@gmail.com>
2018-01-20 13:28:17 +0200

    Drops - make the percent a float and not a number

--- server/lib/drops/drops.controller.ts
+++ server/lib/drops/drops.controller.ts
@@ -1,9 +1,9 @@
 
 import MasterController from '../master/master.controller';
-import { doesChanceWork } from './drops.services';
+import { doesChanceWorkFloat } from './drops.services';
 
 export default class DropsController extends MasterController {
     public isDropped(chance: number): boolean {
-        return doesChanceWork(chance);
+        return doesChanceWorkFloat(chance);
     }
 };
